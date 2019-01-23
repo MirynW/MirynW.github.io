df29cfcb1471858ad5710b476eb43485e778a1dd
Michael R Nickerson <Mirynicks@gmail.com>
2019-01-23 09:40:22 -0500

    somebody

--- NewSite/script.js
+++ NewSite/script.js
@@ -2,26 +2,41 @@ var image = [];
 var i;
 var counter = 0;
 var type = ".jpg";
-imageAmount = 11;
-window.onload = function() {
-  for (i = 1; i<=imageAmount; i++) {
-    image[i] = "images/" + i + type;
-  }
-}
+var slideTime = 3000;
+var buttonPressActive = false;
+imageAmount = 9;
+
 function switchImage() {
+    buttonPressActive = true;
     counter = (counter + 1)%(imageAmount);
-    if (counter == 0) {
-      counter = 1;
-    }
+    console.log(counter);
     document.getElementById("image").src=image[counter];
 }
 
 function goBackImage(){
+  buttonPressActive = true;
   if (counter == 0) {
     counter = image.length-1;
   }
   else {
     counter = (counter-1)%(image.length);
   }
+  console.log(counter);
   document.getElementById("image").src=image[counter];
+}
+
+function loopSlide() {
+  while(buttonPressActive == false) {
+    counter = (counter + 1)%(imageAmount);
+    document.getElementById("image").src=image[counter];
+    console.log("Loop Slide was called with " + image[counter]);
+  }
+}
+
+window.onload = function() {
+  for (i = 0; i<=imageAmount-1; i++) {
+    image[i] = "images/" + i + type;
+    console.log("i is " + i);
+  }
+  setInterval(function(){loopSlide()}, slideTime);
 }
