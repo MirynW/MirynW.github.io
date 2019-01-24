a99bcad5b65d18438fe9af4d90d123b85e9f15a2
Michael R. Nickerson <mirynicks@gmail.com>
2019-01-24 15:47:54 -0500

    s

--- MichaelNickerson/Artwork/slideshow.js
+++ MichaelNickerson/Artwork/slideshow.js
@@ -1,2 +1,23 @@
 var counter = 0;
-var image = ["../images/IpswitchRockPhoto.jpeg", "../images/OceanPhoto.jpeg", "../images/IpswitchRockPhoto.jpeg"];
+var image = ["../images/TrailerIllustrated.jpeg", "../images/OceanPhoto.jpeg", "../images/IpswitchRockPhoto.jpeg"];
+var width = 2040/1.79;
+var height = 1530/1.79;
+
+window.onload = function() {
+  document.getElementById("image").style="width: " + width + "px; height: " + height + "px;"
+}
+
+function nextImage(){
+  counter = (counter+1)%(image.length);
+  document.getElementById("image").src=image[counter];
+}
+
+function previousImage(){
+  if(counter == 0){
+    counter = image.length-1;
+  }
+  else{
+    counter = (counter-1)%(image.length);
+  }
+  document.getElementById("image").src=image[counter];
+}
