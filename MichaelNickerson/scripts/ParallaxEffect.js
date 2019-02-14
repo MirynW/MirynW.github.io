dd3dca995f8b3ea70ef92d88e645e38b41550896
Michael R. Nickerson <mirynicks@gmail.com>
2019-02-14 00:59:45 -0500

    NewSiteAdded

---
+++ MichaelNickerson/scripts/ParallaxEffect.js
@@ -0,0 +1,7 @@
+//Deal with conditions on scroll to affect the transform of the image, image scrolls slower than page
+
+//get doc.container
+
+//get doc.image class
+
+//adjust scroll speed
---
+++ MichaelNickerson/scripts/main.js
@@ -0,0 +1,6 @@
+//Deal with user data and cookies to say welcome/welcome back
+
+//Create a form that takes in user data such as email phone link and other information
+
+//create the drop down list in external js
+
